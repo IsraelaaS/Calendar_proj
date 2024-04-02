da51af68e4171376c5ff6e04f8d994692ab08ee5
Israel Alan Agoe-Sowah <agoesowahia@vcu.edu>
2024-04-02 13:48:19 -0400

    event edit method moved and implemented and option added to main method

--- Event.py
+++ Event.py
@@ -6,20 +6,20 @@ class Event:
         self.start_t= start_t
         self.end_t=end_t
 
+    # Commented out due to redundancy in main class
+    # def edit_event(self, n_name, n_start, n_end,n_date, n_description):
+    #     if len(n_name)>0:
+    #         self.name=n_name
+    #     if len(n_date)>0:
+    #         self.date=n_date
+    #     if len(n_start)>0:
+    #         self.start_t=n_start
+    #     if len(n_end)>0:
+    #         self.end_t=n_end
+    #     self.description=n_description
 
-    def edit_event(self, n_name, n_start, n_end,n_date, n_description):
-        if len(n_name)>0:
-            self.name=n_name
-        if len(n_date)>0:
-            self.date=n_date
-        if len(n_start)>0:
-            self.start_t=n_start
-        if len(n_end)>0:
-            self.end_t=n_end
-        self.description=n_description
-
-    def display(self):
-        print(self)
+    # def display(self):
+    #     print(self)
 
     def __str__(self):
         return f'\nEvent: {self.name}\nDate: {self.date}\nStart Time: {self.start_t}\nEnd Time: {self.end_t}\nDescription: {self.description}'
--- main.py
+++ main.py
@@ -2,48 +2,49 @@ from Event import Event
 import datetime
 
 # list containing all events
-Events=[]
+Events = []
 
 
 def add_events():
-    name= input("Enter name of event: ")
-    date= input_date("Enter date of event (mm/dd/yyyy): ")
-    start= input_time("Enter start time of event (HH:MM): ")
-    end= input_time("Enter end time of even (HH:MM): ")
+    name = input("Enter name of event: ")
+    date = input_date("Enter date of event (mm/dd/yyyy): ")
+    start = input_time("Enter start time of event (HH:MM): ")
+    end = input_time("Enter end time of even (HH:MM): ")
 
     # error check for start time being after end time
     while end <= start:
         print("End time must be after start time.")
-        end= input_time("Enter end time of event (HH:MM): ")
+        end = input_time("Enter end time of event (HH:MM): ")
 
     des_yn = input("Do you want to add a description? Y or N ")
     if des_yn == "Y" or des_yn == 'y':
         description = input("Enter the description: ")
     else:
         description = "None"
-    
-    new_Event= Event(name, date, start, end, description)
+
+    new_Event = Event(name, date, start, end, description)
 
     # Israel
     # Check for duplicate events will be written here
-    if check_dup(new_Event)==False:
+    if check_dup(new_Event) == False:
         Events.append(new_Event)
 
 
 # Searches through events
 def list_all():
-    if len(Events)==0:
+    if len(Events) == 0:
         print("No events")
     else:
         for event in Events:
-            print(event)
+            event.__str__()
             print("\n********************")
 
 
 def list_events_day():
     print("List of events")
 
-# Carter 
+
+# Carter
 # Input date prompt
 # loops to retrieve user input in correct xx/xx/xxxx format
 # returns formatted date
@@ -56,7 +57,8 @@ def input_date(prompt):
         except ValueError:
             print("Invalid date format. Please enter a date in the format 'mm/dd/yyyy'")
 
-#Carter
+
+# Carter
 # Input time prompt
 # loops to retrieve user input in correct xx:xx format
 # returns formatted time
@@ -68,23 +70,48 @@ def input_time(prompt):
         except ValueError:
             print("Invalid time format. Please enter a time in the format 'HH:MM'")
 
+
 # Israel
 # Used to delete events from Event list; should only delete first match
 def delete_event(f_event):
     for event in Events:
         if f_event == event.name:
             Events.remove(event)
-            return print(f_event+" has been deleted\n")
-    
+            return print(f_event + " has been deleted\n")
+
 
 # Israel
 # Method to check duplicate events within Event list
 def check_dup(f_event):
     for event in Events:
         if f_event.__eq__(event):
-            print(f_event.name+" is already registered")
+            print(f_event.name + " is already registered")
             return True
     return False
+
+
+# Israel event editor function
+def edit_event(f_event):
+    for event in Events:
+        if f_event == event.name:
+            c_name = input("Do you want to change name? Y or N")
+            if c_name == "Y":
+                event.name = input("Enter new event name: ")
+            c_date = input("Do you want to change date? Y or N")
+            if c_date == "Y":
+                event.date = input("Enter new event date: ")
+            c_start_time = input("Do you want to change start time? Y or N")
+            if c_start_time == "Y":
+                event.start_t = input("Enter new event start time: ")
+            c_end_time = input("Do you want to change end time? Y or N")
+            if c_end_time == "Y":
+                event.end_t = input("Enter new event end time: ")
+            c_description = input("Do you want to change description? Y or N")
+            if c_description == "Y":
+                event.description = input("Enter new event description: ")
+            return print("Changed event name: " + event.name)
+        else:
+            print("Event not found")
 
 
 def main():
@@ -92,22 +119,24 @@ def main():
     # Carter
     while True:
         print("What would you like to do")
-        user_Answer = input("A Add Event | D Delete Event | L List All | E Exit\n")
+        user_Answer = input("A Add Event | D Delete Event | L List All | E Edit Event | X Exit\n")
 
         if user_Answer == 'A' or user_Answer == 'a':
             add_events()
-        elif user_Answer == 'D' or  user_Answer == 'd':
+        elif user_Answer == 'D' or user_Answer == 'd':
             delete_answer = input("What is the name of the event you want to delete?")
             delete_event(delete_answer)
-        elif user_Answer == 'L' or  user_Answer == 'l':
+        elif user_Answer == 'L' or user_Answer == 'l':
             list_all()
-        elif user_Answer == 'E' or  user_Answer =='e':
+        elif user_Answer == 'E' or user_Answer == 'e':
+            event_find = input("What is the name of the event you want to edit?")
+            edit_event(event_find)
+        elif user_Answer == 'X' or user_Answer == 'x':
             print("Exiting")
             break
         else:
             print("Invalid input try again")
 
-        
 
 if __name__ == "__main__":
     main()
